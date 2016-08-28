d097dc8ac5fbf88a0809307ac7b9fd1eed395c76
Ang Shi Ya <shiya_95@hotmail.com>
2016-08-28 15:31:13 +0800

    Edit Address to use Block, Street, Unit, PostalCode

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -8,12 +8,26 @@ import seedu.addressbook.data.exception.IllegalValueException;
  */
 public class Address {
 
-    public static final String EXAMPLE = "123, some street";
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
-    public static final String ADDRESS_VALIDATION_REGEX = ".+";
+    public static final String ADDRESS_EXAMPLE = Block.BLOCK_EXAMPLE + ", "
+                                               + Street.STREET_EXAMPLE + ", "
+                                               + Unit.UNIT_EXAMPLE + ", "
+                                               + PostalCode.POSTALCODE_EXAMPLE;
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS = 
+            "Person addresses should be in the format block, street, unit, postal code";
+    public static final String ADDRESS_VALIDATION_REGEX = ".+, .+, .+, .+";
+    
+    public static final int ADDRESS_INDEX_BLOCK = 0;
+    public static final int ADDRESS_INDEX_STREET = 1;
+    public static final int ADDRESS_INDEX_UNIT = 2;
+    public static final int ADDRESS_INDEX_POSTALCODE = 3;
 
     public final String value;
     private boolean isPrivate;
+    
+    public final Block block;
+    public final Street street;
+    public final Unit unit;
+    public final PostalCode postalCode;
 
     /**
      * Validates given address.
@@ -25,11 +39,16 @@ public class Address {
         if (!isValidAddress(address)) {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
+        String[] addressDetails = address.split(", ");
+        this.block = new Block(addressDetails[ADDRESS_INDEX_BLOCK]);
+        this.street = new Street(addressDetails[ADDRESS_INDEX_STREET]);
+        this.unit = new Unit(addressDetails[ADDRESS_INDEX_UNIT]);
+        this.postalCode = new PostalCode(addressDetails[ADDRESS_INDEX_POSTALCODE]);
         this.value = address;
     }
 
     /**
-     * Returns true if a given string is a valid person email.
+     * Returns true if a given string is a valid person address.
      */
     public static boolean isValidAddress(String test) {
         return test.matches(ADDRESS_VALIDATION_REGEX);
