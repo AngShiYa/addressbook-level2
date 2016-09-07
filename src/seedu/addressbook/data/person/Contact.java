5f6020bb1f86916927eeb21b13095611a9d750c4
Ang Shi Ya <shiya_95@hotmail.com>
2016-09-07 10:38:26 +0800

    Add Contact and extract commonalities from Address, Email, Phone

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,31 @@
+package seedu.addressbook.data.person;
+
+public abstract class Contact {
+
+    protected String value;
+    protected boolean isPrivate;
+    
+    public Contact() { }
+    
+    public Contact(String value, boolean isPrivate) {
+        this.value = value;
+        this.isPrivate = isPrivate;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public abstract boolean equals(Object other);
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}
